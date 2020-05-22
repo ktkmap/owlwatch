68c8133c4772755b5f0eb8e27e2368dda0a6dd0e
khang <khang.ngo@jhuapl.edu>
2020-05-22 11:39:21 -0400

    owlwatch as class

--- owlwatch.js
+++ owlwatch.js
@@ -1,52 +1,59 @@
 const masterOwlStates={};
 
-// websocket endpoint listening for incoming owl status messages
-function owlWatchService(app,path)
+class OwlWatchService
 {
-    console.log("watching for owls...");
+    constructor(app)
+    {
+        this.app=app;
 
-    app.ws(path,(owlConnection)=>{
-        console.log("owl connected");
+        this.masterOwlStates={};
 
-        owlConnection.on("message",(message)=>{
-            message=JSON.parse(message);
-            masterOwlStates[message.name]=message;
+        this.updateClientConnectionsId=0;
+        this.updateClients={};
+    }
+
+    // websocket service listening for inbound status updates from owls
+    inboundService(path)
+    {
+        console.log("watching for owls...");
+
+        this.app.ws(path,(owlConnection)=>{
+            console.log("owl connected");
+
+            owlConnection.on("message",(message)=>{
+                message=JSON.parse(message);
+                this.masterOwlStates[message.name]=message;
+            });
+
+            owlConnection.on("close",()=>{
+                console.log("owl disconnected");
+            });
         });
 
-        owlConnection.on("close",()=>{
-            console.log("owl disconnected");
-        });
-    });
+        setInterval(()=>{
+            console.log(this.masterOwlStates);
+        },3000);
+    }
 
-    setInterval(()=>{
-        console.log(masterOwlStates);
-    },3000);
+    // websocket outbound service broadcasting owl master state
+    updateService(path)
+    {
+        this.app.ws(path,(owlWatcher)=>{
+            console.log("owl watcher connected");
+
+            var clientid=++this.updateClientConnectionsId;
+            this.updateClients[clientid]=owlWatcher;
+
+            owlWatcher.on("close",()=>{
+                console.log("owl watcher closed");
+                delete this.updateClients[clientid];
+            });
+        });
+
+        setInterval(()=>{
+            console.log("clients",Object.keys(this.updateClients));
+        },3000);
+    }
 }
 
-var updateClientConnectionsId=0;
-const updateClients={};
-
-// service that provides continuous owl state updates to connected clients
-function owlUpdatesService(app,path)
-{
-    app.ws(path,(owlWatcher)=>{
-        console.log("owl watcher connected");
-
-        var clientid=++updateClientConnectionsId;
-        updateClients[clientid]=owlWatcher;
-
-        owlWatcher.on("close",()=>{
-            console.log("owl watcher closed");
-            delete updateClients[clientid];
-        });
-    });
-
-    setInterval(()=>{
-        console.log("clients",Object.keys(updateClients));
-    },3000);
-}
-
-module.exports={
-    owlWatchService,
-    owlUpdatesService
-};
+module.exports=OwlWatchService;
--- testserver.js
+++ testserver.js
@@ -1,14 +1,15 @@
 const express=require("express");
 const expressWs=require("express-ws");
-const {owlWatchService,owlUpdatesService}=require("./owlwatch");
+const OwlWatchService=require("./owlwatch");
 
 function main()
 {
     const app=express();
     expressWs(app);
 
-    owlWatchService(app,"/owlinbound");
-    owlUpdatesService(app,"/owlstatus");
+    var owlWatch=new OwlWatchService(app);
+    owlWatch.inboundService("/owlinbound");
+    owlWatch.updateService("/owlstatus");
 
     app.use(express.static(`${__dirname}/web`));
 
